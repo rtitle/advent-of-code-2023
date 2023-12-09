52a6174566e5525dddee13871c7853c517ad3529
Robert Title <rtitle@broadinstitute.org>
2023-12-09 10:37:12 -0500

    Use reverse

--- src/Day9.hs
+++ src/Day9.hs
@@ -10,14 +10,13 @@ difference :: [Int] -> [Int]
 difference [] = []
 difference is = zipWith (-) (tail is) is
 
-applyDifference :: Bool -> [Int] -> Int
-applyDifference part1 is
+applyDifference :: [Int] -> Int
+applyDifference is
   | length (nub is) == 1 = head is
-  | part1 = (last is) + applyDifference part1 (difference is)
-  | otherwise = (head is) - applyDifference part1 (difference is)
+  | otherwise = (last is) + applyDifference (difference is)
 
 day9 :: String -> (Int, Int)
 day9 input = (part1, part2) where
     ls = fmap parseLine . lines $ input
-    part1 = sum . fmap (applyDifference True) $ ls
-    part2 = sum . fmap (applyDifference False) $ ls
+    part1 = sum . fmap applyDifference $ ls
+    part2 = sum . fmap (applyDifference . reverse) $ ls
