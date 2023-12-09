d9e518462e2984cd9b097b2ce525e6a068ffdd72
Robert Title <rtitle@broadinstitute.org>
2023-12-09 10:52:12 -0500

    Rename function

--- src/Day9.hs
+++ src/Day9.hs
@@ -10,13 +10,13 @@ difference :: [Int] -> [Int]
 difference [] = []
 difference is = zipWith (-) (tail is) is
 
-applyDifference :: [Int] -> Int
-applyDifference is
+extrapolate :: [Int] -> Int
+extrapolate is
   | length (nub is) == 1 = head is
-  | otherwise = (last is) + applyDifference (difference is)
+  | otherwise = (last is) + extrapolate (difference is)
 
 day9 :: String -> (Int, Int)
 day9 input = (part1, part2) where
     ls = fmap parseLine . lines $ input
-    part1 = sum . fmap applyDifference $ ls
-    part2 = sum . fmap (applyDifference . reverse) $ ls
+    part1 = sum . fmap extrapolate $ ls
+    part2 = sum . fmap (extrapolate . reverse) $ ls
